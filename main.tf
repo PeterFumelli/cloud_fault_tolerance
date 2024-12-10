0c70728827b6a2a3e55b4b68f7e5f2a5da12defb
PeterFumelli <pf@hosit.ru>
2024-12-10 18:51:00 +0300

    readme

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+variable "yandex_cloud_token" {
+  type = string
+  description = "Enter your Yandex Cloud token"
+}
+
+provider "yandex" {
+  token     = var.yandex_cloud_token
+  cloud_id  = "your-cloud-id"
+  folder_id = "your-folder-id"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "web-server"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87kbts7j40q5b9rpjr" # Ubuntu 20.04 или другая актуальная ОС
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
